5b489c4f1154689c8223a52db6c2cffc11db9ac0
Anas <adelanas2000@gmail.com>
2024-01-18 19:38:06 +0100

    Creating the controller code for the new user

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -1,48 +1,81 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
 const Post = require('../Schema/PostModel');
 
 const getAllPosts = async (req, res) => {
-    try {
-      const posts = await Post.find();
-      res.json(posts);
-    } catch (err) {
-      console.error('There was an error fetching the Data', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    console.error('There was an error fetching the Data', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
-  const createPost = async (req, res) => {
-    try {
-      const { name, category, ingredients, instructions } = req.body;
-      const newData = await Post.create({ name, category, ingredients, instructions });
-      res.status(201).json(newData);
-    } catch (err) {
-      console.error('There was an error creating the post', err);
-      res.status(400).json({ error: 'Bad Request' });
-    }
-  };
+const createPost = async (req, res) => {
+  try {
+    const { name, category, ingredients, instructions } = req.body;
+    const newData = await Post.create({ name, category, ingredients, instructions });
+    res.status(201).json(newData);
+  } catch (err) {
+    console.error('There was an error creating the post', err);
+    res.status(400).json({ error: 'Bad Request' });
+  }
+};
 
-  const updatePost = async (req, res) => {
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { name, category, ingredients, instructions } = req.body;
+    const updatedPost = await Post.findByIdAndUpdate(id, { name, category, ingredients, instructions }, { new: true });
+    res.json(updatedPost);
+  } catch (err) {
+    console.error('There was an error updating the post', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
     const { id } = req.params;
-    try {
-      const { name, category, ingredients, instructions } = req.body;
-      const updatedPost = await Post.findByIdAndUpdate(id, { name, category, ingredients, instructions }, { new: true });
-      res.json(updatedPost);
-    } catch (err) {
-      console.error('There was an error updating the post', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
+    const deletedPost = await Post.findByIdAndDelete(id);
+    res.json({ message: 'Post deleted successfully', deletedPost });
+  } catch (err) {
+    console.error('There was an error deleting the post', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
-  const deletePost = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const deletedPost = await Post.findByIdAndDelete(id);
-      res.json({ message: 'Post deleted successfully', deletedPost });
-    } catch (err) {
-      console.error('There was an error deleting the post', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
-  
+const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
 
-module.exports = {getAllPosts, createPost, updatePost, deletePost};
+    if (existingUser) {
+      return res.status(409).json({ message: 'This email already exists' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  getAllPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  registerUser,
+};
--- Schema/userModel.js
+++ Schema/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -17,14 +19,40 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    validate: {
-      validator: function (password) {
-        return password.length >= 6;
-      },
-      message: 'The password should contain at least 6 characters',
-    },
   },
 });
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('password')) {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Method to generate JWT token
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, email: this.email },
+    'your-secret-key', // replace with your secret key
+    { expiresIn: '1d' }
+  );
+  return token;
+};
 
 const User = mongoose.model('User', userSchema);
 
