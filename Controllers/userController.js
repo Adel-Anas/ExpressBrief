f01e28942dd5c83a66a3e4406694cc69142d6511
Anas <adelanas2000@gmail.com>
2024-01-19 10:20:09 +0100

    added the user Controller and logout

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,12 +1,14 @@
 const bcrypt = require('bcrypt');
 
-const User = require('../models/userModel');
+const User = require('../Models/userModel')
 
 const jwt = require('jsonwebtoken')
 
+
+
 const registerUser = async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-    try {
+  try {
+      const { firstName, lastName, email, password } = req.body;
       const existingUser = await User.findOne({ email });
   
       if (existingUser) {
@@ -23,11 +25,37 @@ const registerUser = async (req, res) => {
       
       await newUser.save();
   
-      res.status(201).json({ message: 'User registered successfully' });
+      res.status(201).json({ message: 'User registered successfully', newUser });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   };
 
-  module.exports = registerUser;
+  const userLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if(!user){
+        return res.status(401).json({ message: 'Email or Password Incorrect' });
+      }
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if(!validPassword){
+          return res.status(401).json({ message: 'Email or Password Incorrect' });
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.cookie("access_token",token);
+        res.status(200).json({ token, user });
+    }catch(err){
+      console.error(err);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+
+  const userLogout = (req, res) => {
+    res.clearCookie("access_token");
+    res.status(200).json({ message: 'Logged out successfully' });
+  }
+
+module.exports = { registerUser, userLogin, userLogout };
